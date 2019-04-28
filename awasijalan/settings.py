bfc4eea7dc5104ef427c513c553b7e59736e767e
PUYUP <hellopuyup@gmail.com>
2019-04-28 11:00:53 +0700

    Updated!

--- awasijalan/settings.py
+++ awasijalan/settings.py
@@ -162,9 +162,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+"""
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static/'),
 )
+"""
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
